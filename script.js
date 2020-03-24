3ee36777f59bc95729812d04db2868c4a3ec03ca
Chris Barless <chris@barless.co>
2020-03-24 13:26:44 -0400

    Draw the full window width

--- script.js
+++ script.js
@@ -1,25 +1,20 @@
-(function() {
+(function main() {
   const canvas = document.getElementById('canvas');
   const context = canvas.getContext('2d');
-  const hexagonWidth = 25;
+
+  const hexagonLimit = 5765;
+  const rowLimit = Math.ceil((16 / 9) * Math.sqrt(hexagonLimit));
+  const hexagonWidth = Math.floor(canvas.width / rowLimit);
+  const a = hexagonWidth / 4;
+  const b = Math.sqrt(3) * a;
+  const { innerWidth, innerHeight } = window;
+
   let xIndex = 0;
   let yIndex = 0;
-
-  function drawHexagons(numberToDraw) {
-    context.save();
-    context.textAlign = 'center';
-    context.textBaseline = 'middle';
-    for (let i = 0; i < numberToDraw; i++) {
-      drawHexagon(i);
-    }
-    context.restore();
-  }
 
   function drawHexagon(hexIndex) {
     let x = (xIndex + 1) * hexagonWidth;
     const y = (yIndex + 1) * hexagonWidth;
-    const a = hexagonWidth / 4;
-    const b = Math.sqrt(3) * a;
 
     if (yIndex % 2 !== 0) {
       x -= hexagonWidth / 2;
@@ -38,12 +33,25 @@
     context.stroke();
     context.fillText(hexIndex, x, y);
 
-    if (xIndex > 30) {
+    if (xIndex > rowLimit) {
       xIndex = 0;
       yIndex += 1;
     }
-    xIndex++;
+    xIndex += 1;
   }
 
-  drawHexagons(5765);
-})();
+  function drawHexagons(numberToDraw) {
+    context.save();
+    context.textAlign = 'center';
+    context.textBaseline = 'middle';
+    for (let i = 0; i < numberToDraw; i += 1) {
+      drawHexagon(i);
+    }
+    context.restore();
+  }
+
+  canvas.setAttribute('width', innerWidth);
+  canvas.setAttribute('height', innerHeight);
+
+  drawHexagons(hexagonLimit);
+}());
