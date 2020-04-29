c0b0d075bfd7abb1c310b60af848dfe609db39fd
Chris Barless <chris@barless.co>
2020-04-29 13:14:42 -0400

    Fix row wrapping bug (#9)

--- src/components/hexagon-grid.js
+++ src/components/hexagon-grid.js
@@ -3,28 +3,29 @@ import Plane from './plane';
 
 const hexagonLimit = 5765;
 const hexagonWidth = 10;
-const rowLimit = 96;
+const rowLimit = 95;
 const overallWidth = hexagonWidth * rowLimit;
 const overallHeight = hexagonWidth * Math.floor(hexagonLimit / rowLimit);
 
 function HexagonGrid(scene) {
   const hexagons = new Map();
 
-  let xIndex = 0;
-  let yIndex = 0;
+  let column = 1;
+  let row = 1;
 
   for (let hexIndex = 1; hexIndex <= hexagonLimit; hexIndex += 1) {
-    let x = xIndex * hexagonWidth - overallWidth / 2;
-    const y = yIndex * hexagonWidth - overallHeight / 2;
+    let x = column * hexagonWidth - overallWidth / 2;
+    const y = row * hexagonWidth - overallHeight / 2;
+    const isEvenRow = row % 2 === 0;
 
-    if (yIndex % 2 !== 0) {
+    if (isEvenRow) {
       x += hexagonWidth / 2;
     }
-    if (xIndex >= rowLimit) {
-      xIndex = 0;
-      yIndex += 1;
+    if (column >= (isEvenRow ? rowLimit - 1 : rowLimit)) {
+      column = 1;
+      row += 1;
     } else {
-      xIndex += 1;
+      column += 1;
     }
 
     const hex = Hexagon(hexagonWidth, hexIndex);
