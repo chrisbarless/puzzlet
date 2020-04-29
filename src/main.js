2d8792221cd584eed6e630b300b223121748325b
Chris Barless <chris@barless.co>
2020-04-29 14:57:57 -0400

    Handle window resize

--- src/main.js
+++ src/main.js
@@ -27,6 +27,15 @@ function init() {
   document.body.appendChild(stats.dom);
 
   grid = new HexagonGrid(renderer, scene, camera);
+
+  function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+  }
+
+  window.addEventListener('resize', onWindowResize, false);
 }
 
 function animate() {
