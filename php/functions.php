86e8337d198a2826976abd3a97c696086238caf0
Chris Barless <chris@barless.co>
2020-05-01 14:37:44 -0400

    Nicer PHP

--- php/functions.php
+++ php/functions.php
@@ -1,28 +1,26 @@
 <?php
-
-add_action( 'wp_ajax_get_sold_hexes', 'tft_handle_get_sold_hexes' );
-add_action( 'wp_ajax_nopriv_get_sold_hexes', 'tft_handle_get_sold_hexes' );
+add_action('wp_ajax_get_sold_hexes', 'tft_handle_get_sold_hexes');
+add_action('wp_ajax_nopriv_get_sold_hexes', 'tft_handle_get_sold_hexes');
 function get_sold_hexes() {
-  global $wpdb;
+    global $wpdb;
 
-  $pusselbit_key =  'Pusselbit Number';
+    $pusselbit_key = 'Pusselbit Number';
 
-  $response= $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}woocommerce_order_itemmeta WHERE meta_key=\"{$pusselbit_key}\"", OBJECT );
+    $response = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}woocommerce_order_itemmeta WHERE meta_key=\"{$pusselbit_key}\"", OBJECT);
 
-  function mapper($result) {
-    $digit = explode(" ", $result->meta_value)[0];
-    return (int)$digit;
-  }
+    function mapper($result) {
+        $digit = explode(" ", $result->meta_value) [0];
+        return (int)$digit;
+    }
 
-  $response =array_map( "mapper", $response);
+    $response = array_map("mapper", $response);
 
-  return $response;
+    return $response;
 
 }
 function tft_handle_get_sold_hexes() {
-  /* $name	= isset($_POST['name'])?trim($_POST['name']):""; */
-  $hexes = get_sold_hexes();
-  wp_send_json($hexes);
-} 
-
+    /* $name	= isset($_POST['name'])?trim($_POST['name']):""; */
+    $hexes = get_sold_hexes();
+    wp_send_json($hexes);
+}
 
