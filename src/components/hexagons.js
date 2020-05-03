8903a6a8af18e625989c124a49bc971a44487794
Chris Barless <chris@barless.co>
2020-05-03 15:39:35 -0400

    Slightly improve hex drawing

--- src/components/hexagons.js
+++ src/components/hexagons.js
@@ -1,13 +1,18 @@
 import * as THREE from 'three';
 
+const hexRadius = Math.pow(2, 1 / 4) / 2;
+
 const texture = new THREE.TextureLoader().load(
   'http://i.imgur.com/7KAE5M7.jpg',
 );
 
-const geometry = new THREE.CircleBufferGeometry(0.5, 6);
+const geometry = new THREE.CircleBufferGeometry(hexRadius, 6);
 geometry.rotateZ(Math.PI / 2);
 
-const material = new THREE.MeshBasicMaterial({ map: texture });
+const material = new THREE.MeshBasicMaterial({
+  // color: 0xffffff,
+  map: texture,
+});
 
 const Hexagon = (hexCount) => {
   const mesh = new THREE.InstancedMesh(geometry, material, hexCount);
