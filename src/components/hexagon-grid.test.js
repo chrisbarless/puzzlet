8486bd9f4804a2f75b314a10af07dc5716baa6be
Chris Barless <chris@barless.co>
2020-05-09 23:17:34 -0400

    More T

--- src/components/hexagon-grid.test.js
+++ src/components/hexagon-grid.test.js
@@ -8,5 +8,8 @@ describe('Hexagon Grid', () => {
   it('Gets a hex by x,y', () => {
     const grid = new Grid();
     expect(grid.getHexByPosition([0, 0]).bitNumber).toEqual(1);
+    expect(grid.getHexByPosition([1, 0]).bitNumber).toEqual(2);
+    expect(grid.getHexByPosition([0, 1]).bitNumber).toEqual(96);
+    expect(grid.getHexByPosition([94, 60]).bitNumber).toEqual(5765);
   });
 });
