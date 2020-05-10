b0cc71d7a1200aee8cc95e96197e77dd1a344708
Chris Barless <chris@barless.co>
2020-05-09 21:53:21 -0400

    A little more config for linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,13 @@ module.exports = {
     browser: true,
     es6: true,
   },
-  extends: ['airbnb-base'],
+  extends: ['plugin:jest/recommended', 'airbnb-base'],
   plugins: ['jest'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
   },
+  parser: 'babel-eslint',
   parserOptions: {
     ecmaVersion: 2018,
   },
